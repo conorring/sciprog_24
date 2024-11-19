7cb7dc618bfd0baef5d71dda5b7f4b7fa8ac931b
Conor Ring <conor.ring1@ucdconnect.ie>
2024-11-19 18:45:35 +0000

    Code added along with tester files for practical09

---
+++ practical09/README.md
@@ -0,0 +1,10 @@
+# Practical09
+
+This practical deals with the problem of checking if a square is magic or not. For this problem, we dynamically allocated memory for 2D arrays and filled the entries by reading data from a file. The function checking if a square is magic or not is declared in the header file, 'magic_square.h'. The user is aked to input the name of a file containing a square matrix and the program will let you know if it is Magic or not. Two test files have been uploaded also.
+
+# Compiling
+* gcc main_stub.c -o out
+
+# Execute
+./out
+
---
+++ practical09/magic_square.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+// Checks if a matrix is a magic square.
+// A magic square is an n-sided matrix whose sum of values for each
+// row, column, main and secondary diagonals equals to n(n^2 + 1)/2.
+// The function takes as input a matrix 'square' and its side length 'n'
+// and outputs 0 if 'n' is negative or 'square' is NOT a magic square;
+// otherwise, outputs a non-zero value
+//
+
+
+int isMagicSquare(int ** square, const int n) {
+
+    // Eliminate the case where 'n' is negative
+    if(n < 0) {
+        return 0;
+    }
+
+    // M is the sum of every row, column,
+    // and the main and secondary diagonals
+    int M = (n * (n*n + 1))/2;
+
+    int i, j;
+    int rowSums = 0;
+    int colSums = 0;
+    // TODO: Checking that every row and column add up to M
+    for (i = 0; i < n; i++)
+    {
+	rowSums = 0;
+	colSums = 0;
+        for (j = 0; j < n; j++)
+	{
+	    rowSums += square[i][j];
+	    colSums += square[j][i];
+	}
+	if (rowSums != M || colSums != M)
+	{
+	    return 0;
+	}
+    }
+
+    // TODO: Checking that the main and secondary
+    // diagonals each add up to M
+    // If passed all checks, it is a magic square
+
+    int trace = 0;
+    int antiDiag = 0;
+    for (i = 0; i < n; i++)
+    {
+        trace += square[i][i];
+	antiDiag += square[i][n - 1 - i];
+    }
+    if (trace != M || antiDiag != M)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
